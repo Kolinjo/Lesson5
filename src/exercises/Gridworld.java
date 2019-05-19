1fae7280b2c13b0966f421ded309dba47739fa9c
Kolinjo <kozicmarko@yahoo.com>
2019-05-20 00:21:13 +0200

    GridWorld commit

--- src/exercises/Gridworld.java
+++ src/exercises/Gridworld.java
@@ -1,4 +1,13 @@
 package exercises;
+
+import java.awt.Color;
+
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
+
 /* 
  * Use the Gridworld Reference document in the "doc" directory to complete the following tasks;
  * 
@@ -11,4 +20,53 @@ package exercises;
  * 7. Fill the whole world with flowers!
 */
 
-public class Gridworld {}
+public class Gridworld {
+	
+	public static void main(String[] args) {
+		World world = new World ();
+		world.show();
+				
+		Bug bug = new Bug ();
+		Location location = new Location(0,0);
+		
+		world.add(location, bug);
+		
+		Bug bug1 = new Bug();
+		Location location1 = new Location (3,4);
+		
+		world.add(location1, bug1);
+		bug1.setColor(Color.blue);
+		bug1.turn();
+		bug1.turn();
+
+		Flower flowers = new Flower();
+		Flower flowers1 = new Flower ();
+		Flower flowers2 = new Flower ();
+		
+		
+		Location location2 = new Location (3,3);
+		Location location3 = new Location (3,5);
+		
+		world.add(location2, flowers);
+		world.add(location3, flowers);
+		
+		flowers1.setColor(Color.green);
+		flowers2.setColor(Color.orange);
+		
+		
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {			
+			Location location4 = new Location (i,j);
+			world.add(location4, flowers1);
+			}
+			
+//		for (int n = 1; n < 10; n++) {
+//			Location location5 = new Location(n, 0);
+//			world.add(location5, flowers2);
+//			
+//		}
+			
+		}
+	}
+	
+}
